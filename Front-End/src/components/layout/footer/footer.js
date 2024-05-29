32b5f260b9cf25f6693b31b3496c1eb9f23cc90a
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-05-29 16:25:14 +0300

    feat(css): düzeltme

--- Front-End/src/components/layout/footer/footer.js
+++ Front-End/src/components/layout/footer/footer.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import './Footer.css';
+import './footer.css';
 import { Link } from 'react-router-dom';
 import { Context } from '../../../context/Context';
 
